cf5fad8886298d4db3b1dd963ae4aa96b800f4d1
Daniel Olavio <64712584+danielolaviobr@users.noreply.github.com>
2021-03-08 16:26:41 -0300

    Add pino logging and handle errors (#2)
    * Add pino logging and handle errors
    
    * Fixing err.message on logging

--- lib/firestore-admin.ts
+++ lib/firestore-admin.ts
@@ -2,77 +2,57 @@ import { firestore } from "@lib/firebase-admin";
 import { Feedback, Site } from "./@types/firestore";
 
 export async function getAllFeedback(siteId: string) {
-  try {
-    const snapshot = await firestore
-      .collection("feedback")
-      .where("siteId", "==", siteId)
-      .get();
+  const snapshot = await firestore
+    .collection("feedback")
+    .where("siteId", "==", siteId)
+    .get();
 
-    const feedback: Feedback[] = [];
+  const feedback: Feedback[] = [];
 
-    snapshot.forEach((doc) =>
-      feedback.push({ id: doc.id, ...doc.data() } as Feedback)
-    );
+  snapshot.forEach((doc) =>
+    feedback.push({ id: doc.id, ...doc.data() } as Feedback)
+  );
 
-    return feedback;
-  } catch (err) {
-    console.error(err);
-    return [];
-  }
+  return feedback;
 }
 
 export async function getAllSites() {
-  try {
-    const snapshot = await firestore.collection("sites").get();
-    const sites: Site[] = [];
+  const snapshot = await firestore.collection("sites").get();
+  const sites: Site[] = [];
 
-    snapshot.forEach((doc) => {
-      sites.push({ id: doc.id, ...doc.data() } as Site);
-    });
+  snapshot.forEach((doc) => {
+    sites.push({ id: doc.id, ...doc.data() } as Site);
+  });
 
-    return sites;
-  } catch (err) {
-    console.error(err);
-    return [];
-  }
+  return sites;
 }
 
 export async function getUsersFeedback(uid: string) {
-  try {
-    const snapshot = await firestore
-      .collection("feedback")
-      .where("authorId", "==", uid)
-      .get();
+  const snapshot = await firestore
+    .collection("feedback")
+    .where("authorId", "==", uid)
+    .get();
 
-    const feedback: Feedback[] = [];
+  const feedback: Feedback[] = [];
 
-    snapshot.forEach((doc) =>
-      feedback.push({ id: doc.id, ...doc.data() } as Feedback)
-    );
+  snapshot.forEach((doc) =>
+    feedback.push({ id: doc.id, ...doc.data() } as Feedback)
+  );
 
-    return feedback;
-  } catch (err) {
-    console.error(err);
-    return [];
-  }
+  return feedback;
 }
 
 export async function getUserSites(uid: string) {
-  try {
-    const snapshot = await firestore
-      .collection("sites")
-      .where("authorId", "==", uid)
-      .get();
+  const snapshot = await firestore
+    .collection("sites")
+    .where("authorId", "==", uid)
+    .get();
 
-    const sites: Site[] = [];
+  const sites: Site[] = [];
 
-    snapshot.forEach((doc) => {
-      sites.push({ id: doc.id, ...doc.data() } as Site);
-    });
+  snapshot.forEach((doc) => {
+    sites.push({ id: doc.id, ...doc.data() } as Site);
+  });
 
-    return sites;
-  } catch (err) {
-    console.error(err);
-    return [];
-  }
+  return sites;
 }
--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { auth } from "@lib/firebase-admin";
 import { getUsersFeedback } from "@lib/firestore-admin";
+import logger, { prepObjectKeys } from "@utils/logger";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -10,6 +11,19 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     return res.json({ feedback });
   } catch (err) {
+    logger.error(
+      {
+        request: {
+          headers: prepObjectKeys(req.headers),
+          url: req.url,
+          method: req.method
+        },
+        response: {
+          statusCode: res.statusCode
+        }
+      },
+      err.message
+    );
     return res.status(500).json({ ...err });
   }
 };

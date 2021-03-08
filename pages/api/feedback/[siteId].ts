1616146e55b98dfcbdd5f6d0b6bd0b7d0f5ed1e7
Daniel <danielolaviobr@gmail.com>
2021-03-08 16:17:57 -0300

    Add pino logging and handle errors

--- pages/api/feedback/[siteId].ts
+++ pages/api/feedback/[siteId].ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getAllFeedback } from "@lib/firestore-admin";
+import logger, { prepObjectKeys } from "@utils/logger";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { siteId } = req.query as { [key: string]: string };
@@ -8,6 +9,19 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     return res.json({ feedback });
   } catch (err) {
+    logger.error(
+      {
+        request: {
+          headers: prepObjectKeys(req.headers),
+          url: req.url,
+          method: req.method
+        },
+        response: {
+          statusCode: res.statusCode
+        }
+      },
+      JSON.stringify(err)
+    );
     res.status(500).json({ ...err });
   }
 };
